c12f41846c14a37a1596734c356f94775d9d4573
daniarnaizg <daniarnaizg@gmail.com>
2023-05-17 14:54:50 +0200

    Added email sender

--- scraper.py
+++ scraper.py
@@ -1,6 +1,62 @@
 import json
 import requests
+import argparse
 from bs4 import BeautifulSoup
+
+def send_simple_message(new_items, api_key):
+
+    # html body that shows the new products in the dicionary new_items. as a table showing images, name and prices
+    html_body = f'''
+    <html>
+        <body>
+        <h1>{len(list(new_items.keys()))} new products in Supersonido outlet!</h1>
+
+    '''
+
+    for key in new_items.keys():
+
+        url = new_items[key]['url']
+        image = new_items[key]['image']
+        name = new_items[key]['name']
+        price = float(new_items[key]['price'].replace(",", "."))
+        price_new = float(new_items[key]['price_new'].replace(",", "."))
+        sale_percentage = round((price - price_new) / price * 100, 2)
+
+        html_body += f'''
+                <a href="{url}">
+                    <h3>{name}</h3>
+                </a>
+                <span>{price_new} €</span>
+                <span> → </span>
+                <span>{price} €</span>
+                <span style="font-size: 30px; color: red;">{sale_percentage}%</span> 
+                <br>
+                <img src="{image}" height="250">
+                <br>
+        '''
+
+    html_body += '''
+
+        </body>
+    </html>
+    '''
+
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox275ac03099e1436ea6627decb4301641.mailgun.org/messages",
+        auth=("api", api_key),
+        data={"from": "Supersonido Outlet <postmaster@sandbox275ac03099e1436ea6627decb4301641.mailgun.org>",
+              "to": "Dani Arnaiz <daniarnaizg@gmail.com>",
+              "subject": f"{len(list(new_items.keys()))} new products from Supersonido!",
+              "text": f"{len(list(new_items.keys()))} new items in Supersonido outlet!",
+              "html": html_body})
+
+
+if __name__ == '__main__':
+
+    # get api key as argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("api_key", help="Mailgun API key")
+    args = parser.parse_args()
 
 HEADERS = {
     'Access-Control-Allow-Origin': '*',
@@ -42,31 +98,39 @@ for i in range(len(product_names)):
     new_products[name] = {
         "name": name,
         "price": product_prices[i].text.strip().split(" ")[0],
-        "price_new": product_prices[i].text.strip().split(" ")[1].split("â\x82¬")[1].replace("\n","").strip() if "â\x82¬" in product_prices[i].text.strip() else "",
+        "price_new": product_prices[i].text.strip().split(" ")[1].split("â\x82¬")[1].replace("\n", "").strip() if "â\x82¬" in product_prices[i].text.strip() else "",
         "url": f"www.supersonido.es{product_urls[i]['href']}",
         "image": f"www.supersonido.es{product_images[i]['style'].replace('background-image: url(','').replace(');','')}"
     }
 
 
 # new item test
-new_products["new_item"] = {
-        "name": "new_item",
-        "price": "12",
-        "price_new": "14",
-        "url": f"www.supersonido.es",
-        "image": f"www.supersonido.es"
-    }
+new_products["AudioPro ADDON C5A833"] = {
+    "name": "AudioPro ADDON C5A",
+    "price": "199,00",
+    "price_new": "269,00",
+    "url": "www.supersonido.es/p/audiopro-addon-c5a__",
+    "image": "www.supersonido.es/productos/imagenes/producto33149.jpg"
+}
+
+new_products["Supra Cables Regleta Lorad MD06-EU/SP83"] = {
+        "name": "Supra Cables Regleta Lorad MD06-EU/SP",
+        "price": "199,00",
+        "price_new": "230,00",
+        "url": "www.supersonido.es/p/supra-cables-regleta-lorad-md06-eusp",
+        "image": "www.supersonido.es/productos/imagenes/producto1476.jpg"
+}
 
 # check if there is a new product by checking keys
 # if there is a new product, send a message to the user
+new_deals = {}
 for key in list(new_products.keys()):
     if key not in list(products_old.keys()):
-        print(f"New product: {key}")
+        new_deals[key] = new_products[key]
+        # print(f"New product: {key}")
 
-# print(new_products[list(new_products.keys())[0]])
-# print(new_products[list(new_products.keys())[1]])
-# print(new_products[list(new_products.keys())[2]])
-# print('â\x82¬\n339,00'.strip())
+if new_deals.keys():
+    send_simple_message(new_deals, args.api_key)
 
 # save as json
 with open("./products.json", "w", encoding='UTF-8') as file_new:
