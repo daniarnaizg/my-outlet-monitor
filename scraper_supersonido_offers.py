a8e754bf790014d085124f10846940241d63f296
Dani Arnaiz <daniarnaizg@gmail.com>
2025-02-15 15:08:48 +0100

    updatet number of offer pages to scrape to 5

--- scraper_supersonido_offers.py
+++ scraper_supersonido_offers.py
@@ -2,7 +2,7 @@ from supersonido_utils import OffersScraper, HEADERS, BASE_URL
 
 if __name__ == '__main__':
     parser = OffersScraper.get_common_parser("Supersonido Offers Scraper")
-    parser.add_argument("--pages", type=int, default=2, help="Number of pages to scrape")
+    parser.add_argument("--pages", type=int, default=5, help="Number of pages to scrape")
     parser.add_argument("--discount", type=int, default=40, 
                       help="Minimum discount percentage to notify")
     args = parser.parse_args()
